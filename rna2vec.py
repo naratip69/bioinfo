568334374fe5411883c2ce1a24bd4180fa7e43da
naratip chakansin <naratip.18257@gmail.com>
2023-11-18 20:51:51 +0700

    adding flatten ,droup out and more epoch

--- rna2vec.py
+++ rna2vec.py
@@ -13,10 +13,11 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 # from gcgc import KmerTokenizer
 # from protein2vec import proteinTokenizer,translate,read_fa
 from sklearn.model_selection import train_test_split,KFold
-from keras.layers import Input, Concatenate, Conv1D,MaxPool1D,Embedding,Lambda,Dense
+from keras.layers import Input, Concatenate, Conv1D,MaxPool1D,Embedding,Lambda,Dense,Flatten,Dropout
 from keras.models import Model
+from keras.optimizers import Adam
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import accuracy_score,recall_score,precision_score,balanced_accuracy_score,matthews_corrcoef
+from sklearn.metrics import accuracy_score,recall_score,precision_score,balanced_accuracy_score,matthews_corrcoef,classification_report,confusion_matrix
 # import esm
 from transformers import T5Tokenizer, T5EncoderModel
 import torch
@@ -163,20 +164,22 @@ def get_model():
     protein_input = Input((2048,1,),name='protein_input')
     rnaEmbed = Embedding(input_dim=4097,output_dim=25,weights=[embedding_rna_weights],trainable=True)(rna_input)
     # proteinEmbed = Embedding(input_dim=248,output_dim=343,trainable=True)(protein_input)
-    conv_pro = Conv1D(filters=512,use_bias=True,kernel_size=4)(protein_input)
-    conv_rna = Conv1D(filters=512,use_bias=True,kernel_size=4)(rnaEmbed)
-    pro_pool = MaxPool1D(pool_size=48,strides=2,padding='valid')(conv_pro)
-    rna_pool = MaxPool1D(pool_size=48,strides=2,padding='valid')(conv_rna)
-    conv_pro2 = Conv1D(filters=64,use_bias=True,kernel_size=4)(pro_pool)
-    conv_rna2 = Conv1D(filters=64,use_bias=True,kernel_size=4)(rna_pool)
-    pro_pool2 = MaxPool1D(pool_size=30,strides=3)(conv_pro2)
-    rna_pool2 = MaxPool1D(pool_size=30,strides=3)(conv_rna2)
+    conv_pro = Conv1D(filters=32,use_bias=True,kernel_size=4)(protein_input)
+    conv_rna = Conv1D(filters=32,use_bias=True,kernel_size=4)(rnaEmbed)
+    pro_pool = MaxPool1D(pool_size=48,strides=4,padding='valid')(conv_pro)
+    rna_pool = MaxPool1D(pool_size=50,strides=2,padding='valid')(conv_rna)
+    conv_pro2 = Conv1D(filters=49,use_bias=True,kernel_size=4)(pro_pool)
+    conv_rna2 = Conv1D(filters=49,use_bias=True,kernel_size=4)(rna_pool)
+    pro_pool2 = MaxPool1D(pool_size=32,strides=4)(conv_pro2)
+    rna_pool2 = MaxPool1D(pool_size=24,strides=4)(conv_rna2)
     feature_fusion = Lambda(matrix_mul)((pro_pool2,rna_pool2)) # (?,48,128) , (?,48,128)
-    fully_conect = Dense(128,activation='relu',use_bias=True)(feature_fusion)
-    m2 = Dense(64,activation='relu',use_bias=True)(fully_conect)
-    m3 = Dense(32,activation='relu',use_bias=True)(m2)
-    m4 = Dense(16,activation='relu',use_bias=True)(m3)
-    output = Dense(1,activation='softplus')(m4)
+    flat = Flatten()(feature_fusion)
+    fully_conect = Dense(64,activation='relu',use_bias=True)(flat)
+    m1 = Dropout(0.5)(fully_conect)
+    m2 = Dense(32,activation='relu',use_bias=True)(m1)
+    m3 = Dropout(0.5)(m2)
+    m5 = Dense(1,activation='softplus')(m3)
+    output = Dense(1,activation='sigmoid',name='output')(m5)
     model = Model(inputs=[rna_input,protein_input],outputs=output)
     # print(model.summary())
     return model
@@ -188,6 +191,7 @@ X = data_set.drop('lpis',axis=1).values
 y = data_set['lpis'].values
 y = [int(e=='PositivePair') for e in y]
 y = np.array(y)
+y.astype('float32')
 y.reshape(-1)
 print(y.shape)
 #print(X.shape) # (3243, 2)
@@ -200,64 +204,119 @@ print(y.shape)
 # print(maxlen) #3391
 
 # print(rna_array.shape,protein_array.shape) # (3243,496) (3243,1415)
-# X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
-# model = get_model()
-cv = KFold(n_splits=5,shuffle=True,random_state=42)
-fold_no = 1
-metrics_per_fold = []
-show = False
-for train, test in cv.split(X,y):
-    print("     ")
-    print(f'Training for fold {fold_no}')
+train_X,test_X,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42,stratify=y)
+rna_train = rnaTokenizer(train_X[:,1])
+protein_train = (train_X[:,0])
+rna_test = rnaTokenizer(test_X[:,1])
+protein_test = (test_X[:,0])
+print(test_X.shape,y_test.shape)
+print(f'negative: {np.sum(y_train==0)} , positive : {np.sum(y_train)}')
+print(torch.cuda.is_available())
 
-    # scaler = MinMaxScaler()
-    train_X = X[train]
-    test_X = X[test]
-    rna_train = rnaTokenizer(train_X[:,1])
-    protein_train = (train_X[:,0])
-    rna_test = rnaTokenizer(test_X[:,1])
-    protein_test = (test_X[:,0])
-    print(test_X.shape,y[test].shape)
-    print(torch.cuda.is_available())
-
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    # if torch.cuda.is_available():
-    #     torch.set_default_tensor_type(torch.cuda.HalfTensor)
-    tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_half_uniref50-enc', do_lower_case=False)
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+# if torch.cuda.is_available():
+# torch.set_default_tensor_type(torch.cuda.HalfTensor)
+tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_half_uniref50-enc', do_lower_case=False)
 
 # Load the model
-    pretrain_model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc").to(device)
-    pretrain_model.full() if device=='cpu' else pretrain_model.half()
-    ids_train = tokenizer(list(protein_train), add_special_tokens=True, padding="longest")
-    ids_test = tokenizer(list(protein_test), add_special_tokens=True, padding="longest")
+pretrain_model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc").to(device)
+pretrain_model.full() if device=='cpu' else pretrain_model.half()
+ids_train = tokenizer(list(protein_train), add_special_tokens=True, padding="longest")
+ids_test = tokenizer(list(protein_test), add_special_tokens=True, padding="longest")
 
-    input_ids_train = torch.tensor(ids_train['input_ids']).to(device)
-    input_ids_test = torch.tensor(ids_test['input_ids']).to(device)
-    attention_mask_train = torch.tensor(ids_train['attention_mask']).to(device)
-    attention_mask_test = torch.tensor(ids_test['attention_mask']).to(device)
-    with torch.no_grad():
-        embedding_repr_train = pretrain_model(input_ids=input_ids_train, attention_mask=attention_mask_train)
-        embedding_repr_test = pretrain_model(input_ids=input_ids_test, attention_mask=attention_mask_test)
-    # print(embedding_repr.last_hidden_state.shape) #torch.Size([1622, 2, 1024])
-    emb_train = embedding_repr_train.last_hidden_state.cpu().numpy()
-    emb_test = embedding_repr_test.last_hidden_state.cpu().numpy()
-    emb_train = emb_train.reshape((emb_train.shape[0],-1))
-    emb_test = emb_test.reshape((emb_test.shape[0],-1))
-    # emb = emb.reshape((emb.shape[0],-1))
-    # print(emb.shape) #(1622, 2048)
-    model = get_model()
-    model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-    if(not show):
-        print(model.summary())
-        show = True
+input_ids_train = torch.tensor(ids_train['input_ids']).to(device)
+input_ids_test = torch.tensor(ids_test['input_ids']).to(device)
+attention_mask_train = torch.tensor(ids_train['attention_mask']).to(device)
+attention_mask_test = torch.tensor(ids_test['attention_mask']).to(device)
+with torch.no_grad():
+    embedding_repr_train = pretrain_model(input_ids=input_ids_train, attention_mask=attention_mask_train)
+    embedding_repr_test = pretrain_model(input_ids=input_ids_test, attention_mask=attention_mask_test)
+# print(embedding_repr.last_hidden_state.shape) #torch.Size([1622, 2, 1024])
+emb_train = embedding_repr_train.last_hidden_state.cpu().numpy()
+emb_test = embedding_repr_test.last_hidden_state.cpu().numpy()
+emb_train = emb_train.reshape((emb_train.shape[0],-1))
+emb_test = emb_test.reshape((emb_test.shape[0],-1))
+# emb = emb.reshape((emb.shape[0],-1))
+# print(emb.shape) #(1622, 2048)
+model = get_model()
+print(model.summary())
+adam = Adam(learning_rate=0.00001)
+model.compile(loss='binary_crossentropy',optimizer=adam,metrics=['accuracy'])
 
-    history = model.fit([rna_train,emb_train],y[train],batch_size=12,epochs=32,verbose=1)
 
-    model.save('models/model_fold_'+str(fold_no)+".h5")
+
+model.fit([rna_train,emb_train],y_train,batch_size=32,epochs=100,verbose=1)
+
+model.save('models/model.h5')
+
+y_pred = model.predict([rna_test,emb_test])
+y_pred.reshape(-1)
+y_pred = y_pred > 0.5
+y_pred.astype('float32')
+print(y_pred.shape,y_test.shape)
+print(y_pred[:5])
+print(y_test[:5])
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+acc1 = accuracy_score(y_test,y_pred) 
+print(acc1)
+# cv = KFold(n_splits=2,shuffle=True,random_state=42)
+# fold_no = 1
+# metrics_per_fold = []
+# show = False
+# for train, test in cv.split(X,y):
+#     print("     ")
+#     print(f'Training for fold {fold_no}')
+
+#     # scaler = MinMaxScaler()
+#     train_X = X[train]
+#     test_X = X[test]
+#     rna_train = rnaTokenizer(train_X[:,1])
+#     protein_train = (train_X[:,0])
+#     rna_test = rnaTokenizer(test_X[:,1])
+#     protein_test = (test_X[:,0])
+#     print(test_X.shape,y[test].shape)
+#     print(f'negative: {np.sum(y[test]==0)} , positive : {np.sum(y[test])}')
+#     print(torch.cuda.is_available())
+
+#     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+#     # if torch.cuda.is_available():
+#     #     torch.set_default_tensor_type(torch.cuda.HalfTensor)
+#     tokenizer = T5Tokenizer.from_pretrained('Rostlab/prot_t5_xl_half_uniref50-enc', do_lower_case=False)
+
+# # Load the model
+#     pretrain_model = T5EncoderModel.from_pretrained("Rostlab/prot_t5_xl_half_uniref50-enc").to(device)
+#     pretrain_model.full() if device=='cpu' else pretrain_model.half()
+#     ids_train = tokenizer(list(protein_train), add_special_tokens=True, padding="longest")
+#     ids_test = tokenizer(list(protein_test), add_special_tokens=True, padding="longest")
+
+#     input_ids_train = torch.tensor(ids_train['input_ids']).to(device)
+#     input_ids_test = torch.tensor(ids_test['input_ids']).to(device)
+#     attention_mask_train = torch.tensor(ids_train['attention_mask']).to(device)
+#     attention_mask_test = torch.tensor(ids_test['attention_mask']).to(device)
+#     with torch.no_grad():
+#         embedding_repr_train = pretrain_model(input_ids=input_ids_train, attention_mask=attention_mask_train)
+#         embedding_repr_test = pretrain_model(input_ids=input_ids_test, attention_mask=attention_mask_test)
+#     # print(embedding_repr.last_hidden_state.shape) #torch.Size([1622, 2, 1024])
+#     emb_train = embedding_repr_train.last_hidden_state.cpu().numpy()
+#     emb_test = embedding_repr_test.last_hidden_state.cpu().numpy()
+#     emb_train = emb_train.reshape((emb_train.shape[0],-1))
+#     emb_test = emb_test.reshape((emb_test.shape[0],-1))
+#     # emb = emb.reshape((emb.shape[0],-1))
+#     # print(emb.shape) #(1622, 2048)
+#     model = get_model()
+#     model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
+#     if(not show):
+#         print(model.summary())
+#         show = True
+
+#     history = model.fit([rna_train,emb_train],y[train],batch_size=32,epochs=32,verbose=1)
+
+#     model.save('models/model_fold_'+str(fold_no)+".h5")
     
-    _,score = model.evaluate([rna_test,emb_test],y[test],verbose=0)
-    metrics_per_fold.append(score)
-    fold_no = fold_no + 1
+#     _,score = model.evaluate([rna_test,emb_test],y[test],verbose=0)
+#     metrics_per_fold.append(score)
+#     fold_no = fold_no + 1
 
-for score in metrics_per_fold:
-    print(score)
+# for score in metrics_per_fold:
+#     print(score)
